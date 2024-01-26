95a88563ebaab29d7fcfad8b6de8e0adc66e62a7
Doyle Brown <dalpiad@gmail.com>
2024-01-25 20:05:32 -0600

    Studio mostly done with bonus!! (woot woot)

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,20 +1,44 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotNull
+    @Size(min = 5, max = 15, message = "The username must be between 5 and 15 characters.")
     private String username;
+
+    @Email
     private String email;
+
+    @NotNull
+    @Size(min = 6, message = "The password must be at least 6 characters long.")
     private String password;
 
+    @NotNull(message = "Passwords must match.")
+    private String verifyPassword;
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
+
+    private void checkPassword(){
+        if (password != null || verifyPassword != null) {
+            if (!(password.equals(verifyPassword))) {
+                verifyPassword = null;
+            }
+        }
+    }
+
 
     public String getUsername() {
         return username;
@@ -38,6 +62,16 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
 
